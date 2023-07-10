b395a40cb8ab559c0a72aff27dfcef1163ce18f9
v.nesterenko <v.nesterenko@dreamsoft.by>
2023-07-10 17:24:35 +0300

    KS-1-3 Flight data print

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main(args: Array<String>) {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print("[$hour:$minute]")
+
+}
