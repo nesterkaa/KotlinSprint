b648ba3c7d0031d53c03900d9c1b38c725f92199
v.nesterenko <v.nesterenko@dreamsoft.by>
2023-07-10 17:15:50 +0300

    KS-1-2 added new variables and print

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package lesson_1
+
+fun main(args: Array<String>) {
+    val numberOfOrder: Int = 75
+    val feedback: String = "Спасибо за чудесный продукт"
+
+    println(numberOfOrder)
+    println(feedback)
+
+    var numberOfWorkers = 2000
+//    println(2000)
+
+    println(--numberOfWorkers)
+
+}
