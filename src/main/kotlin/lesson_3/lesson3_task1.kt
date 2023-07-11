c985062eb0f563db7e68148a53dc964914694e5b
v.nesterenko <v.nesterenko@dreamsoft.by>
2023-07-11 10:16:54 +0300

    KS-3-1 added greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,9 @@
+package lesson_3
+
+fun main( ){
+    val userName = "Виктория"
+    var greeting = "Добрый день, $userName"
+    println(greeting)
+    greeting = "Добрый вечер, $userName"
+    println(greeting)
+}
