bc2493e393b392b093bd516407227ed656a2b685
v.nesterenko <v.nesterenko@dreamsoft.by>
2023-07-11 09:59:57 +0300

    KS-2-5 calculated deposit amount

---
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,16 @@
+package lesson_2
+
+import kotlin.math.pow
+
+fun main( ){
+    val clientSum = 70000.0
+    println("Введите процентную ставку")
+    val percentRate  = readln().toDouble()
+    println("Введите промежуток времени (в годах)")
+    val times  = readln().toInt()
+
+    val resultSum = clientSum * (1 + percentRate/100.0).pow(times)
+
+    println(String.format("%.3f",resultSum))
+
+}
