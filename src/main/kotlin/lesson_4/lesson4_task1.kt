17f3dfb09d326ad2da056d3ac04a0a85d4ed09d5
v.nesterenko <v.nesterenko@dreamsoft.by>
2023-07-12 12:25:42 +0300

    KS-4-1 create const

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,10 +1,12 @@
 package lesson_4
 
 fun main() {
-    val tables = 13
+
     val reservationToday = 13
     val reservationTomorrow = 9
 
-    println("Доступность столиков на сегодня: ${tables != reservationToday} \nДоступность столиков на завтра: ${tables != reservationTomorrow}")
+    println("Доступность столиков на сегодня: ${TABLES != reservationToday} \nДоступность столиков на завтра: ${TABLES != reservationTomorrow}")
 
 }
+
+const val TABLES = 13
